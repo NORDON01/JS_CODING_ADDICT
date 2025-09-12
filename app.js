43a10227a9157468b1352e21b9580f5cb800bf25
NORDON01 <georges.nordon@gmail.com>
2025-09-12 23:25:49 +0200

    A jour jusqu'à 'Events'

--- app.js
+++ app.js
@@ -1,32 +1,15 @@
 const log = console.log;    //global scope
 
+const random = document.querySelector('.random');
+log(random);
 
-//Create empty 'div'
-const bodyDiv = document.createElement('div');
-//Create text node
-const text = document.createTextNode('A simple body div');
-//Append the text node to the bodyDiv
-bodyDiv.append(text);
-document.body.append(bodyDiv);
-bodyDiv.classList.add('blue');      //CSS
-log(bodyDiv);
+/* random.style.background = 'blue';
+random.style.color = 'white';
+random.style.fontSize = '3rem';
+random.style.textTransform = 'capitalize';
+random.style.width = '90%';
+random.style.textAlign = 'center'; */
 
-const result = document.getElementById('result');
-log(result); 
+//Plus rapide:
+random.classList.add('title');
 
-const heading = document.createElement('h2');
-const textHeading = document.createTextNode('dynamic heading');
-heading.append(textHeading);
-
-document.body.insertBefore(heading, result); 
-heading.classList.add('red');       //CSS
-//document.body'.insertBefore(heading, bodyDiv);
-log(heading);
-let smallHeading = document.createElement('h6');
-let smallHeadingText =document.createTextNode('I am small heading');
-smallHeading.append(smallHeadingText);
-smallHeading.classList.add('red');
-document.body.append(smallHeading);
-
-document.body.replaceChild(smallHeading, bodyDiv);
-log(smallHeading);
