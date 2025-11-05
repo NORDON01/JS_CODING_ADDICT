98e2a10b8ffe4853b74b1158f7e2d7f3f14dac31
NORDON <georges.nordon@gmail.com>
2025-11-05 13:52:39 +0100

    closure simple

--- app.js
+++ app.js
@@ -1,16 +1,11 @@
 const log = console.log;    //global scope
 
-const btn = document.querySelector('.increment'); log(btn);
- 
-const counter = {
-  count:0,
-  increment(){
-    log(this);
-    this.count++
-    log(this.count);
+function newAccount(name, newBalance){
+  let balance = newBalance;
+  function showBalance(){
+    log(`${name}, your balance is ${balance}`);
   }
+  return showBalance;
 }
 
-let compteur = counter.increment.bind(counter);//!!!
-
-btn.addEventListener('click', compteur);
+log(newAccount('Georges', 1000)());
