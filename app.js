fb959d0367d9ecd3e6ec94ee783bc85f228dd3bf
NORDON <georges.nordon@gmail.com>
2025-11-05 15:15:09 +0100

    closure un exemple complet

--- app.js
+++ app.js
@@ -5,7 +5,26 @@ function newAccount(name, newBalance){
   function showBalance(){
     log(`${name}, your balance is ${balance}`);
   }
-  return showBalance;
-}
 
-log(newAccount('Georges', 1000)());
+  function deposit(amount){
+    balance += amount; 
+  }
+
+  function withDraw(amount){
+    if((balance - amount) < 0 ){log('Balance is insufficient');}
+    else{balance -= amount}
+    } 
+    return {showBalance: showBalance, deposit:deposit, withDraw:withDraw};
+  }
+
+/* log(newAccount('Georges', 1000)());
+log(newAccount('Hélène', 2000)()); */
+// Suite
+const alice = newAccount('Alice', 3000);
+log(alice);
+alice.showBalance();
+alice.deposit(1000);
+alice.showBalance();
+alice.withDraw(500);
+alice.showBalance();
+alice.withDraw(5000);
