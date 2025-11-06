1de2236c88f0d9c163c253b35d51a6efeae827bf
NORDON <georges.nordon@gmail.com>
2025-11-06 16:37:23 +0100

    Template literals & HTML

--- app.js
+++ app.js
@@ -1,30 +1,8 @@
 const log = console.log;    //global scope
 
-function newAccount(name, newBalance){
-  let balance = newBalance;
-  function showBalance(){
-    log(`${name}, your balance is ${balance}`);
-  }
+const fname = 'Bob';
+const lname = 'Sanders';
+const age = 25;
 
-  function deposit(amount){
-    balance += amount; 
-  }
-
-  function withDraw(amount){
-    if((balance - amount) < 0 ){log('Balance is insufficient');}
-    else{balance -= amount}
-    } 
-    return {showBalance: showBalance, deposit:deposit, withDraw:withDraw};
-  }
-
-/* log(newAccount('Georges', 1000)());
-log(newAccount('Hélène', 2000)()); */
-// Suite
-const alice = newAccount('Alice', 3000);
-log(alice);
-alice.showBalance();
-alice.deposit(1000);
-alice.showBalance();
-alice.withDraw(500);
-alice.showBalance();
-alice.withDraw(5000);
+let phrase = `My ${fname} is ${lname.toUpperCase()} and I am ${age*2} years old`;
+log(phrase);
