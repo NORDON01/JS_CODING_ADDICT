37b4bed792f01cf2915ffe612b983a5b8ee6a685
NORDON <georges.nordon@gmail.com>
2025-11-06 20:55:02 +0100

    Template literals & HTML

--- app.js
+++ app.js
@@ -1,15 +1,2 @@
 const log = console.log;    //global scope
 
-const person = {
-    name: 'Kyle',
-    job: 'developer',
-    hobbies: ['surfing', 'baking', 'bowling']
-}
-
-const result = document.getElementById('result'); log(result);
-result.innerHTML = `
-<h2>${person.name}</h2>
-<p>${person.job}</p>
-<ul>${person.hobbies.map(
-    item => {return `<li>${item}</li>`}
-).join("")}</ul>`
