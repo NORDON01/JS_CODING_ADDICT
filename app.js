0355cc6a79865844fe63655216c458140c47b26c
NORDON <georges.nordon@gmail.com>
2025-11-07 09:48:28 +0100

    Tagged template literals

--- app.js
+++ app.js
@@ -1,2 +1,19 @@
 const log = console.log;    //global scope
 
+const author = "Some author";
+const statement = "Some statement";
+
+const quote = highlight `Here is the statement ${statement} by ${author} and it could not be more true`;
+log(quote);
+
+const result = document.getElementById('result'); log(result);
+result.innerHTML = quote;
+
+function highlight(text, ...vars){      //rest operator
+    log({text, vars});
+    const awesomeText = text.map((item, index) => {
+        return `${item} <strong class="blue"> ${vars[index] || ""} </strong>`
+    })
+
+    return awesomeText.join("");
+}
