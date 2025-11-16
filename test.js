23f2e8ececed92598efde8288d49cb8bcdffa3b6
NORDON <georges.nordon@gmail.com>
2025-11-16 16:14:24 +0100

    String inccludes

--- test.js
+++ test.js
@@ -1,11 +1,17 @@
 const log = console.log;
 
-const headings = document.querySelectorAll('h1'); 
-log(headings);  //Displays a node list which cannot take map() method
-const result = document.getElementById('result');
+const products = [
+    {title:'Modern poster'},
+    {title:'Bar stool'},
+    {title:'Armchair'},
+    {title:'Leather chair'}
+];log(products);
 
-const text = [...headings]
-.map(item => `<span>${item.textContent}</span>`)
-.join(" ");
+const text = 'a';
 
-result.innerHTML = text;
+const result = products.filter((item) => 
+    item.title.toLowerCase().includes(text)); //Ne pas utiliser {} après =>
+log(result);            //0: {title: 'Bar stool'}
+                        //1: {title: 'Armchair'}
+                        //2: {title: 'Leather chair'}
+
